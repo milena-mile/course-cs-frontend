37fe7fb3ee71f8189f3e036deca3889e1544f420
Elena Senyutich <elenasenyutich@MacBook-Pro-Elena-3.local>
2023-04-07 02:13:11 +0300

    initial commit

--- lesson-1/index.ts
+++
@@ -1,9 +0,0 @@
-import { bitGetterSetter } from "./bitGetterSetter";
-
-const bitGetter = bitGetterSetter(new Uint8Array([0b1110, 0b1101]));
-console.log(bitGetter.get(0, 1)); // 1
-console.log(bitGetter.get(1, 1)); // 0
-
-const bitAccessor = bitGetterSetter(new Uint8Array([0b1110, 0b1101]));
-console.log(bitAccessor.set(0, 1, 0)); //
-console.log(bitAccessor.get(0, 1)); // 0
--- lesson-1/types.ts
+++
@@ -1,4 +0,0 @@
-export interface Bit {
-    get(arrIndex: number, getBit: number): number,
-    set(arrIndex: number, getBit: number, setBit: number): void
-}
